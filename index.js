489b877b105146e6e0e44060f2fe473bc1b5ae14
Shanae <sleslie18@gmail.com>
2022-02-20 16:24:36 -0500

    Initial Commit

---
+++ index.js
@@ -0,0 +1,40 @@
+let userInput = prompt("What would you like to do?").toLowerCase();
+const toDoList = [];
+
+while (userInput) {
+  if (userInput === "new") {
+    userInput = prompt("Okay, please enter new toDo");
+    toDoList.push(userInput);
+    console.log(`${userInput} added to list`);
+  }
+
+  if (userInput === "list") {
+    console.log("**********");
+    for (let i = 0; i < toDoList.length; i++) {
+      console.log(`${i}: ${toDoList[i]}`);
+    }
+    console.log("**********");
+  }
+
+  if (userInput === "delete") {
+    input = prompt("Enter the index of the toDo you want deleted");
+    toDoList.splice([input], 1);
+    console.log("toDo Removed");
+  }
+
+  if (userInput === "quit") {
+    console.log("OK, EXITING THE APP NOW");
+    break; // **prevents infinite loop**
+  }
+
+  userInput = prompt("What would you like to do now?");
+
+  if (
+    userInput !== "new" &&
+    userInput !== "list" &&
+    userInput !== "delete" &&
+    userInput !== "quit"
+  ) {
+    alert("Sorry, I don't recognize that entry, please");
+  }
+}
